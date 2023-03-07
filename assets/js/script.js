236008f1dfbf6c64c3de7d3c6803441a56c3a246
alessiaBer <alexdeyn@gmail.com>
2023-03-07 15:35:11 +0100

    [add] questions and assign them to a variable

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,28 +6,22 @@ Decrizione passaggi:
 -Infine scrivi sulla pagina il risultato usando questo formato: nomecognomecolorepreferito23 
 ******/
 
-// ask user name
+// ask user name and assign it to a variable
 const userName = prompt('What\'s your name?');
-
-// then assign it to a variable
-
 // log it to the console
+console.log(userName);
 
 
-
-//ask user last name
-
-// then assign it to a variable
-
+//ask user last name and assign it to a variable
+const userLastName = prompt('What\'s your last name?');
 // log it to the console
+console.log(userLastName);
 
 
-//ask user his/her favorite color
-
-// then assign it to a variable
-
+//ask user his/her favorite color and assign it to a variable
+const favoriteColor = prompt('What\'s your favorite color?');
 // log it to the console
-
+console.log(favoriteColor);
 
 
 // wrtite down on the page the result password
