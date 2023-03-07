a0bc1b7fa6e41271ced6f046e608d267028a49b4
alessiaBer <alexdeyn@gmail.com>
2023-03-07 15:15:20 +0100

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+/*Decrizione passaggi:
+Chiedi all'utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina il risultato usando questo formato: nomecognomecolorepreferito23 */
