67da12163f0157b9a1f9a13a684c6cd1873a1b6b
alessiaBer <alexdeyn@gmail.com>
2023-03-07 16:59:14 +0100

    [add] pwdgen_plus features

---
+++ pwdgen_plus/myscript.js
@@ -0,0 +1,28 @@
+const button = document.getElementById('generate');
+
+button.addEventListener("click", askData)
+// ask user name and assign it to a variable
+function askData() {
+const userName = prompt('What\'s your name?');
+// log it to the console
+console.log(userName);
+
+
+//ask user last name and assign it to a variable
+const userLastName = prompt('What\'s your last name?');
+// log it to the console
+console.log(userLastName);
+
+
+//ask user his/her favorite color and assign it to a variable
+const favoriteColor = prompt('What\'s your favorite color?');
+// log it to the console
+console.log(favoriteColor);
+
+
+/* write down on the page the result password */
+// assign the HTML element to a variable
+const resultPassword = document.getElementById('result-password');
+// assign value to the HTML element
+resultPassword.innerHTML = `${userName}${userLastName}${favoriteColor}23`;
+};
