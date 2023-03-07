f4b906c20da387e4fb5d5d1a6d47fd23203692ae
alessiaBer <alexdeyn@gmail.com>
2023-03-07 15:28:46 +0100

    [add] exercise steps

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,33 @@
-/*Decrizione passaggi:
-Chiedi all'utente il suo nome,
-poi chiedi il suo cognome,
-poi chiedi il suo colore preferito
-Infine scrivi sulla pagina il risultato usando questo formato: nomecognomecolorepreferito23 */
+/*****
+Decrizione passaggi:
+-Chiedi all'utente il suo nome,
+-poi chiedi il suo cognome,
+-poi chiedi il suo colore preferito
+-Infine scrivi sulla pagina il risultato usando questo formato: nomecognomecolorepreferito23 
+******/
+
+// ask user name
+const userName = prompt('What\'s your name?');
+
+// then assign it to a variable
+
+// log it to the console
+
+
+
+//ask user last name
+
+// then assign it to a variable
+
+// log it to the console
+
+
+//ask user his/her favorite color
+
+// then assign it to a variable
+
+// log it to the console
+
+
+
+// wrtite down on the page the result password
